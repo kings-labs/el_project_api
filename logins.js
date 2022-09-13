7d46ce2d076cb4316b2c3d99514b392918354c38
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-13 20:31:03 +0200

    hid logins

--- logins.js
+++ logins.js
@@ -1,8 +1,8 @@
 const dbConfig = {
-  user: "sa",
-  password: "KingsLabs@22/23#",
-  server: "198.244.247.123",
-  database: "enlightenlearning",
+  user: "username",
+  password: "pwd",
+  server: "ip",
+  database: "dbName",
   port: 1433,
   trustServerCertificate: true,
   encrypt: false,
