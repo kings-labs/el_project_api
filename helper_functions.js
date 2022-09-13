4e061b95f89ac209d9150aa95dc9e32d0a6c914a
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-13 15:36:32 +0200

    added some logic

---
+++ helper_functions.js
@@ -0,0 +1,74 @@
+module.exports = {
+  /**
+   * Algorithm taken from: https://www.geeksforgeeks.org/find-number-of-days-between-two-given-dates/
+   *
+   * @returns true if the class date started within 10 days or less, false if later
+   */
+  isLessThanTenDaysAgo: function (date) {
+    // Split the date parameter into day, month and year
+    const passedDay = date.split("/")[0];
+    const passedMonth = date.split("/")[1];
+    const passedYear = date.split("/")[2];
+    // Split today's date into day, month and year
+    const today = new Date();
+    const currentDay = today.getDate();
+    const currentMonth = today.getMonth() + 1; // +1 because the format is 0-11
+    const currentYear = today.getFullYear();
+    // To store number of days in all months from January to Dec.
+    const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // A function for leap year checking
+    function countLeapYears(year, month) {
+      // used for leap years checking
+      let years = year;
+
+      // Check if the current year needs to be considered
+      // for the count of leap years or not
+      if (month <= 2) {
+        years--;
+      }
+
+      // A year is a leap year if it is a multiple of 4, multiple of 400 and not a multiple of 100.
+      return (
+        Math.floor(years / 4) -
+        Math.floor(years / 100) +
+        Math.floor(years / 400)
+      );
+    }
+
+    // This function returns number of days between two given dates
+    function getDifference(pD, pM, pY, cD, cM, cY) {
+      // COUNT TOTAL NUMBER OF DAYS BEFORE THE PASSED DATE
+      // initialize count using years and day
+      let passedCount = Number(pY) * 365 + Number(pD);
+
+      // Add days for months in given date
+      for (let i = 0; i < pM - 1; i++) {
+        passedCount = Number(passedCount) + monthDays[i];
+      }
+      // Since every leap year is of 366 days, add a day for every leap year
+      passedCount = Number(passedCount) + countLeapYears(pY, pM);
+
+      // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE TODAY
+      let currentCount = Number(cY) * 365 + Number(cD);
+      for (let i = 0; i < cM - 1; i++) {
+        currentCount = Number(currentCount) + monthDays[i];
+      }
+      currentCount = Number(currentCount) + countLeapYears(cY, cM);
+
+      // return difference between two counts
+      return currentCount - passedCount;
+    }
+
+    // The days difference between the given date and today
+    const difference = getDifference(
+      passedDay,
+      passedMonth,
+      passedYear,
+      currentDay,
+      currentMonth,
+      currentYear
+    );
+    return difference <= 10 && difference >= 0;
+  },
+};
--- queries/classes.js
+++ queries/classes.js
@@ -1,49 +1,73 @@
 /**
  * This file holds the code of all queries related to the Classes table.
- * 
+ *
  * @version 11/09/2022
  */
 
+const helperFunctions = require("../helper_functions");
+
 module.exports = {
-    /**
-     * Checks if a class with a given ID actually exists.
-     * 
-     * If the test is successful, the callback function will be called. 
-     * 
-     * @param {*} sql The mssql instance connected to the database currently used by the API.
-     * @param {*} res The object to send result to a given query sender.
-     * @param {*} classID The ID to check the number of classes for. 
-     * @param {*} callback The callback function to be called if the test is successful.
-     */
-    checkIfClassExistsWithID: function(sql,res,classID, callback) {
-        const request = new sql.Request();
-        request
-        .input('classId', sql.Int, classID)
-        .query("SELECT COUNT(ID) FROM CLASSES WHERE ID=@classId", function(err, recordset) {
-            if (err) {
-                console.log(err);
-                res.status(400).json({error:err});
-                return null;
+  /**
+   * Checks if a class with a given ID actually exists.
+   *
+   * If the test is successful, the callback function will be called.
+   *
+   * @param {*} sql The mssql instance connected to the database currently used by the API.
+   * @param {*} res The object to send result to a given query sender.
+   * @param {*} classID The ID to check the number of classes for.
+   * @param {*} callback The callback function to be called if the test is successful.
+   */
+  checkIfClassExistsWithID: function (sql, res, classID, callback) {
+    const request = new sql.Request();
+    request
+      .input("classId", sql.Int, classID)
+      .query(
+        "SELECT COUNT(ID) FROM CLASSES WHERE ID=@classId",
+        function (err, recordset) {
+          if (err) {
+            console.log(err);
+            res.status(400).json({ error: err });
+            return null;
+          } else {
+            if (Object.values(recordset.recordset[0])[0] === 1) {
+              callback();
             } else {
-                if (Object.values(recordset.recordset[0])[0] === 1) {
-                    callback()
-                } else {
-                    console.log("Class does not exist or similar issue.");
-                    res.status(400).json({error:"There is not class with that ID."});
-                }
+              console.log("Class does not exist or similar issue.");
+              res
+                .status(400)
+                .json({ error: "There is not class with that ID." });
             }
-        });
-    },
+          }
+        }
+      );
+  },
 
-    getTutorClasses: function(sql, res, tutorDiscordId) {
-        const request = new sql.Request();
-        request
-        .input('discordUsername', sql.NVarChar, tutorDiscordId)
-        .query("SELECT * FROM CLASSES INNER JOIN COURSES on CLASSES.CourseID = COURSES.ID WHERE COURSES.TutorID = (SELECT ID FROM TUTORS WHERE DiscordUserName=@discordUsername)", function(err, recordset) {
-            if (err) console.log(err);
+  getTutorClasses: function (sql, res, tutorDiscordId) {
+    const request = new sql.Request();
+    request
+      .input("discordUsername", sql.NVarChar, tutorDiscordId)
+      .query(
+        "SELECT COURSES.Level, CLASSES.Date, COURSES.Subject, STUDENTS.FirstName, STUDENTS.LastName, CLASSES.ID FROM CLASSES INNER JOIN COURSES on CLASSES.CourseID = COURSES.ID INNER JOIN STUDENTS on STUDENTS.ID = COURSES.StudentID WHERE COURSES.TutorID = (SELECT ID FROM TUTORS WHERE DiscordUserName=@discordUsername)",
+        function (err, recordset) {
+          if (err) console.log(err);
 
-            const classes = recordset.recordset;
-            
-        })
-    }
-}
+          const classes = recordset.recordset;
+          const returnValues = [];
+          for (const aClass of classes) {
+            if (helperFunctions.isLessThanTenDaysAgo(aClass.Date)) {
+              const returnValueForClass = {
+                name: aClass.Level + " " + aClass.Subject,
+                student: aClass.FirstName + ", " + aClass.LastName,
+                date: aClass.Date,
+                id: aClass.ID,
+              };
+
+              returnValues.push(returnValueForClass);
+            }
+          }
+
+          res.send(returnValues);
+        }
+      );
+  },
+};
