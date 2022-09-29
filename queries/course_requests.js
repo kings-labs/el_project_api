97df6117972797c91a54db13cda6324870d28504
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-29 16:58:13 +0100

    Uncomment updating course requests

--- queries/course_requests.js
+++ queries/course_requests.js
@@ -26,7 +26,7 @@ module.exports = {
             result: formatResult(recordset.recordset),
           });
 
-          //updateCourseRequests(sql);
+          updateCourseRequests(sql);
         }
       }
     );
